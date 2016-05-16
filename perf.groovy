3b1efeb9c24bbb0841703c8c8aab516c774bbc32
Matt Mitchell <mmitche@microsoft.com>
2016-05-16 14:56:22 -0700

    Initial commit of test perf groovy

---
+++ perf.groovy
@@ -0,0 +1,19 @@
+// Import the utility functionality.
+import jobs.generation.Utilities;
+import jobs.generation.JobReport;
+
+def project = GithubProject
+def branch = GithubBranchName
+def gitUrl = Utilities.calculateGitURL(project)
+
+def newJob = workflowJob('test-workflow') {
+    definition {
+        cps {
+            script("""
+                git url: $gitUrl
+                echo 'Hello world'
+            """
+            )
+        }
+    }
+}
