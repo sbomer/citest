a6b4761b7c8bd7bc70bdeed5260baaa8c59f0541
Matt Mitchell <mmitche@microsoft.com>
2016-06-02 14:41:09 -0700

    Initial perf stability

--- perf.groovy
+++ perf.groovy
@@ -1,17 +1,30 @@
 // Import the utility functionality.
-import jobs.generation.Utilities;
-import jobs.generation.JobReport;
+import jobs.generation.*;
 
 def project = GithubProject
 def branch = GithubBranchName
 def gitUrl = Utilities.calculateGitURL(project)
 
-def newJob = workflowJob('test-workflow') {
+// Define the nightly workflows that measure the stability of performance machines
+// Put these in a folder
+
+def stabilityTestingFolderName = 'stability_testing'
+folder('stability_testing') {}
+
+def nativeStabilityJob = workflowJob(Utilities.getFullJobName('native_stability_test', false, stabilityTestingFolderName)) {
+    // Add a parameter to specify which nodes to run the stabiliyt job across
+    parameters {
+        labelParam('NODE_LABEL') {
+            allNodes('allCases', 'IgnoreOfflineNodeEligibility')
+            description('Nodes to run the stability job across')
+        }
+    }
     definition {
         cps {
             script("""
-                git url: '$gitUrl'
-                echo 'Hello world'
+                node {
+                    echo 'Hello world'
+                }
             """
             )
             sandbox()
