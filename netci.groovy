474fc2aab99c5c5107444c6afb6031750c38f697
Matt Mitchell <mmitche@microsoft.com>
2016-06-02 14:41:37 -0700

    Remove test jobs

--- netci.groovy
+++ netci.groovy
@@ -5,23 +5,3 @@ import jobs.generation.Utilities;
 def project = GithubProject
 def branch = GithubBranchName
 
-// Standard build jobs
-
-[true, false].each { isPR ->
-    ['A', 'B', 'C', 'D', 'E', 'F'].each { letter ->
-        def newJob = job(Utilities.getFullJobName(project, "innerloop_${letter}", isPR)) {
-            steps {
-                batchFile("build.cmd")
-            }
-        }
-        
-        Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto')
-        Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
-        if (isPR) {
-            Utilities.addGithubPRTriggerForBranch(newJob, branch, "Say Hello${letter}")
-        }
-        else {
-            Utilities.addGithubPushTrigger(newJob)
-        }
-    }
-}
